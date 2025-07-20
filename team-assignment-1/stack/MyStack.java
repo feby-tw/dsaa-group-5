0777ffee9bbe0d7ac59325018525cc9704de0523
uwi0 <kakapoweb@gmail.com>
2025-07-20 10:10:26 +0700

    feat: update stack using custom stack

---
+++ team-assignment-1/stack/MyStack.java
@@ -0,0 +1,50 @@
+package stack;
+
+import java.util.EmptyStackException;
+
+public class MyStack<T> {
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> top;
+    private int size = 0;
+
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return size;
+    }
+}
